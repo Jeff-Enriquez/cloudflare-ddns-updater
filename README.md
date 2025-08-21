2036813d7238f670eebd1d17259c675bf6e827ee
Jeff-Enriquez <jeff.enriquez.12.28@gmail.com>
2025-08-20 18:32:13 -0700

    update ip4 template to reduce api calls, rotate services, and update readme

--- README.md
+++ README.md
@@ -1,41 +1,27 @@
 # Cloudflare Dynamic DNS IP Updater
-<img alt="GitHub" src="https://img.shields.io/github/license/K0p1-Git/cloudflare-ddns-updater?color=black"> <img alt="GitHub last commit (branch)" src="https://img.shields.io/github/last-commit/K0p1-Git/cloudflare-ddns-updater/main"> <img alt="GitHub contributors" src="https://img.shields.io/github/contributors/K0p1-Git/cloudflare-ddns-updater">
+<img alt="GitHub" src="https://img.shields.io/github/license/Jeff-Enriquez/cloudflare-ddns-updater?color=black"> <img alt="GitHub last commit (branch)" src="https://img.shields.io/github/last-commit/Jeff-Enriquez/cloudflare-ddns-updater/main"> <img alt="GitHub contributors" src="https://img.shields.io/github/contributors/Jeff-Enriquez/cloudflare-ddns-updater">
+
+> I have forked this repo for my own personal use. I reduced the number of api calls to CloudFlare. I spread the load to the IP services. I also removed the slack and discord features since I do not need them. If you manually update the CloudFlare IP address and are running the cron job. You will need to delete the OLD_IP_FILE.
 
 This script is used to update Dynamic DNS (DDNS) service based on Cloudflare! Access your home network remotely via a custom domain name without a static IP! Written in pure BASH.
 
-## Support Me
+## Usage
+This script is used with crontab. Specify the frequency of execution through crontab. My recommendation is every minute.
+1. Use cloudflare-template.sh for IPv4. Update the variables for your CloudFlare account. This is a great video for finding those values - [NetworkChuck](https://www.youtube.com/watch?v=rI-XxnyWFnM&ab_channel=NetworkChuck).
+2. Open the terminal.
+3. Execute `chmod +x {file}`
+   - Example: `chmod +x ./cloudflare.sh`
+4. Execute `crontab -e` this will open up a file.
+5. In the file enter `*/1 * * * * cd {directory - not filename} && /bin/sh ./{file}`. This will run the script every minute. Specifying the directory path allows the OLD_IP_FILE and IP_SERVICES_FILE to be created in the same folder as the sh file.
+   - Example: `*/1 * * * * cd /home/jeff/cloudflare-ddns-updater && /bin/sh ./cloudflare.sh`
+6. Close and save the file.
+7. Execute `systemctl restart cron`. This will restart the cron service.
+
+## Support The Original Creator
+Shout out to the original creator of this project [Jason K](https://github.com/K0p1-Git). This is his link from when I cloned the repo.
 [![Donate Via Paypal](https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif)](https://www.paypal.me/Jasonkkf)
 
-## Installation
-
-```bash
-git clone https://github.com/K0p1-Git/cloudflare-ddns-updater.git
-```
-
-## Usage
-This script is used with crontab. Specify the frequency of execution through crontab.
-
-```bash
-# ┌───────────── minute (0 - 59)
-# │ ┌───────────── hour (0 - 23)
-# │ │ ┌───────────── day of the month (1 - 31)
-# │ │ │ ┌───────────── month (1 - 12)
-# │ │ │ │ ┌───────────── day of the week (0 - 6) (Sunday to Saturday 7 is also Sunday on some systems)
-# │ │ │ │ │ ┌───────────── command to issue                               
-# │ │ │ │ │ │
-# │ │ │ │ │ │
-# * * * * * /bin/bash {Location of the script}
-```
-
-## Tested Environments:
-macOS Mojave version 10.14.6 (x86_64) <br />
-AlmaLinux 9.3 (Linux kernel: 5.14.0 | x86_64) <br />
-Debian Bullseye 11 (Linux kernel: 6.1.28 | aarch64) <br />
-
-## Contributing
-Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
-
-## Reference
+### Reference
 This script was made with reference from [Keld Norman](https://www.youtube.com/watch?v=vSIBkH7sxos) video.
 
 ## License
--- cloudflare-template.sh
+++ cloudflare-template.sh
@@ -1,132 +1,123 @@
 #!/bin/bash
-## change to "bin/sh" when necessary
 
 auth_email=""                                       # The email used to login 'https://dash.cloudflare.com'
-auth_method="token"                                 # Set to "global" for Global API Key or "token" for Scoped API Token
+auth_method="global"                                # Set to "global" for Global API Key or "token" for Scoped API Token
 auth_key=""                                         # Your API Token or Global API Key
 zone_identifier=""                                  # Can be found in the "Overview" tab of your domain
 record_name=""                                      # Which record you want to be synced
 ttl=3600                                            # Set the DNS TTL (seconds)
 proxy="false"                                       # Set the proxy to true or false
-sitename=""                                         # Title of site "Example Site"
-slackchannel=""                                     # Slack Channel #example
-slackuri=""                                         # URI for Slack WebHook "https://hooks.slack.com/services/xxxxx"
-discorduri=""                                       # URI for Discord WebHook "https://discordapp.com/api/webhooks/xxxxx"
-
+previous_ip_file="previousIpAddress.txt"            # Filename to save your old IP address. The file will be created in the same folder as this file. The purpose of this file is to reduce the number of api calls to CloudFlare.
+ip_services_file="ipServicesFile.txt"               # Filename to save the list of IP services. This list is rotated upon every execution, thus evenly spreading out the requests and reducing the number of calls to each individual service.
 
 ###########################################
-## Check if we have a public IP
+## Get previously saved IP address
 ###########################################
+
+# Create the file if it does not exist
+if [[ ! -f "$IP_SERVICES_FILE" ]]; then
+  touch "$IP_SERVICES_FILE"
+  echo "https://api.ipify.org" > "$IP_SERVICES_FILE"
+  echo "https://ipv4.icanhazip.com" >> "$IP_SERVICES_FILE"
+  echo "https://ipinfo.io/ip" >> "$IP_SERVICES_FILE"
+fi
+
+# Create the file if it does not exist
+if [[ ! -f "$OLD_IP_FILE" ]]; then
+    touch "$OLD_IP_FILE"
+fi
+
+# Get the previously saved IP address from the file
+if [[ -f "$OLD_IP_FILE" ]]; then
+    OLD_IP=$(cat "$OLD_IP_FILE")
+fi
+
+###########################################
+## Get current IP address
+###########################################
+
+# Load IP_SERVICES from file
+IP_SERVICES=()
+while IFS= read -r line; do
+  IP_SERVICES+=("$line")
+done < $IP_SERVICES_FILE
+
+# Rotate list
+FIRST_SERVICE=${IP_SERVICES[0]}
+IP_SERVICES=("${IP_SERVICES[@]:1}" "$FIRST_SERVICE")
+
+# Save rotated list back to file
+printf "%s\n" "${IP_SERVICES[@]}" > "$IP_SERVICES_FILE"
+
 REGEX_IPV4="^(0*(1?[0-9]{1,2}|2([0-4][0-9]|5[0-5]))\.){3}0*(1?[0-9]{1,2}|2([0-4][0-9]|5[0-5]))$"
-IP_SERVICES=(
-  "https://api.ipify.org"
-  "https://ipv4.icanhazip.com"
-  "https://ipinfo.io/ip"
-)
 
-# Try all the ip services for a valid IPv4 address
+# Try all the IP services for a valid IPv4 address
 for service in ${IP_SERVICES[@]}; do
-  RAW_IP=$(curl -s $service)
-  if [[ $RAW_IP =~ $REGEX_IPV4 ]]; then
+  raw_ip=$(curl -s $service)
+  if [[ $raw_ip =~ $REGEX_IPV4 ]]; then
     CURRENT_IP=$BASH_REMATCH
-    logger -s "DDNS Updater: Fetched IP $CURRENT_IP"
     break
-  else
-    logger -s "DDNS Updater: IP service $service failed."
   fi
 done
 
 # Exit if IP fetching failed
 if [[ -z "$CURRENT_IP" ]]; then
-  logger -s "DDNS Updater: Failed to find a valid IP."
+  logger -s "DDNS Updater: Failed to retrieve IP."
   exit 2
+fi
+
+# Exit if current IP is the same as the previous
+if [[ "$OLD_IP" == "$CURRENT_IP" ]]; then
+    logger "DDNS Updater: No update required. Your IP address has not changed."
+    exit 0
 fi
 
 ###########################################
 ## Check and set the proper auth header
 ###########################################
-if [[ "${auth_method}" == "global" ]]; then
-  auth_header="X-Auth-Key:"
+if [[ "${AUTH_METHOD}" == "global" ]]; then
+  AUTH_HEADER="X-Auth-Key:"
 else
-  auth_header="Authorization: Bearer"
+  AUTH_HEADER="Authorization: Bearer"
 fi
 
 ###########################################
 ## Seek for the A record
 ###########################################
-
-logger "DDNS Updater: Check Initiated"
-record=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records?type=A&name=$record_name" \
-                      -H "X-Auth-Email: $auth_email" \
-                      -H "$auth_header $auth_key" \
+RECORD=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$ZONE_IDENTIFIER/dns_records?type=A&name=$RECORD_NAME" \
+                      -H "X-Auth-Email: $AUTH_EMAIL" \
+                      -H "$AUTH_HEADER $AUTH_KEY" \
                       -H "Content-Type: application/json")
-
+                      
 ###########################################
 ## Check if the domain has an A record
 ###########################################
-if [[ $record == *"\"count\":0"* ]]; then
-  logger -s "DDNS Updater: Record does not exist, perhaps create one first? (${CURRENT_IP} for ${record_name})"
+if [[ $RECORD == *"\"count\":0"* ]]; then
+  logger -s "DDNS Updater: Record does not exist, perhaps create one first?"
   exit 1
-fi
-
-###########################################
-## Get existing IP
-###########################################
-old_ip=$(echo "$record" | sed -E 's/.*"content":"(([0-9]{1,3}\.){3}[0-9]{1,3})".*/\1/')
-# Compare if they're the same
-if [[ $CURRENT_IP == $old_ip ]]; then
-  logger "DDNS Updater: IP ($CURRENT_IP) for ${record_name} has not changed."
-  exit 0
 fi
 
 ###########################################
 ## Set the record identifier from result
 ###########################################
-record_identifier=$(echo "$record" | sed -E 's/.*"id":"([A-Za-z0-9_]+)".*/\1/')
+RECORD_IDENTIFIER=$(echo "$RECORD" | sed -E 's/.*"id":"([A-Za-z0-9_]+)".*/\1/')
 
 ###########################################
 ## Change the IP@Cloudflare using the API
 ###########################################
-update=$(curl -s -X PATCH "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records/$record_identifier" \
-                     -H "X-Auth-Email: $auth_email" \
-                     -H "$auth_header $auth_key" \
+UPDATE=$(curl -s -X PATCH "https://api.cloudflare.com/client/v4/zones/$ZONE_IDENTIFIER/dns_records/$RECORD_IDENTIFIER" \
+                     -H "X-Auth-Email: $AUTH_EMAIL" \
+                     -H "$AUTH_HEADER $AUTH_KEY" \
                      -H "Content-Type: application/json" \
-                     --data "{\"type\":\"A\",\"name\":\"$record_name\",\"content\":\"$CURRENT_IP\",\"ttl\":$ttl,\"proxied\":${proxy}}")
+                     --data "{\"type\":\"A\",\"name\":\"$RECORD_NAME\",\"content\":\"$CURRENT_IP\",\"ttl\":$TTL,\"proxied\":${PROXY}}")
 
 ###########################################
-## Report the status
+## Save update locally
 ###########################################
-case "$update" in
-*"\"success\":false"*)
-  echo -e "DDNS Updater: $CURRENT_IP $record_name DDNS failed for $record_identifier ($CURRENT_IP). DUMPING RESULTS:\n$update" | logger -s 
-  if [[ $slackuri != "" ]]; then
-    curl -L -X POST $slackuri \
-    --data-raw '{
-      "channel": "'$slackchannel'",
-      "text" : "'"$sitename"' DDNS Update Failed: '$record_name': '$record_identifier' ('$CURRENT_IP')."
-    }'
-  fi
-  if [[ $discorduri != "" ]]; then
-    curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST \
-    --data-raw '{
-      "content" : "'"$sitename"' DDNS Update Failed: '$record_name': '$record_identifier' ('$CURRENT_IP')."
-    }' $discorduri
-  fi
-  exit 1;;
-*)
-  logger "DDNS Updater: $CURRENT_IP $record_name DDNS updated."
-  if [[ $slackuri != "" ]]; then
-    curl -L -X POST $slackuri \
-    --data-raw '{
-      "channel": "'$slackchannel'",
-      "text" : "'"$sitename"' Updated: '$record_name''"'"'s'""' new IP Address is '$CURRENT_IP'"
-    }'
-  fi
-  if [[ $discorduri != "" ]]; then
-    curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST \
-    --data-raw '{
-      "content" : "'"$sitename"' Updated: '$record_name''"'"'s'""' new IP Address is '$CURRENT_IP'"
-    }' $discorduri
-  fi
-  exit 0;;
-esac
+if [[ $UPDATE == *"\"success\":true"* ]]; then 
+    printf "%s" "$CURRENT_IP" > "$OLD_IP_FILE"
+    logger "DDNS Updater: Successfully updated IP address."
+    echo "success"
+else
+    logger -s "DDNS Updater: Failed to update IP address."
+fi
